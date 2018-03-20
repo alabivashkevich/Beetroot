366c4abb3f25635e875c2a07274450296baf5776
Matviekin Ivan <alabivashkevic@gmail.com>
2018-03-21 00:25:40 +0200

    Exam-2 is ready

---
+++ Exam-2/gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    notify = require('gulp-notify'),
+    sourcemaps = require('gulp-sourcemaps');
+
+/* Plugin for webserver*/
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+
+
+
+
+// sass
+gulp.task('sass', function() {
+    return gulp.src(['./src/sass/main.sass'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./src/css'))
+        .pipe(notify("--Done--!"))
+        .pipe(reload({ stream: true }));
+});
+
+//html
+gulp.task('html', function() {
+    gulp.src(['./src/index.html'])
+        .pipe(reload({ stream: true }));
+
+});
+
+//watch
+gulp.task('watch', function() {
+    gulp.watch(
+        [
+            './src/sass/main.sass',
+            './src/sass/section/*.sass',
+            './src/sass/base/*.sass',
+        ], ['sass']);
+
+    gulp.watch(['./src/index.html'], ['html']);
+});
+
+/*Task for webserver*/
+const config = {
+    server: {
+        baseDir: "./src"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Ivan_Matvieikin"
+};
+gulp.task('webserver', () => browserSync(config));
+
+
+gulp.task('default', ['sass', 'html', 'webserver', 'watch']);
---
+++ Exam-2/src/js/main.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+    $(function() {
+        //Initialize filterizr with default options
+        $('#filtr-container').filterizr();
+    });
+    $('#team__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="team__slider-btn team__slider-btn--prev"><span class="ion-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="team__slider-btn team__slider-btn--next"><span class="ion-chevron-right"></span></button>',
+        autoplay: false,
+        // arrows: false,
+        autoplaySpeed: 4000,
+        fade: true,
+    });
+
+    $('#testimonials__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="testimonials__slider-btn testimonials__slider-btn--prev"><span class="ion-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="testimonials__slider-btn testimonials__slider-btn--next"><span class="ion-chevron-right"></span></button>',
+        autoplay: false,
+        // arrows: false,
+        autoplaySpeed: 4000,
+        fade: true,
+    });
+
+    $(function() {
+        $('.team__item-description').slimScroll({
+            height: '250px',
+            color: 'lightgreen'
+        });
+    });
+
+
+
+});
+/*Скролл к выбранному блоку*/
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
+
+// Плавная прокрутка якоря
+$(document).ready(function() {
+    $('#top').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
+// Появление-исчезновение якоря
+$(function(f) {
+    var element = f('.up-uncor');
+    f(window).scroll(function() {
+        element['fade' + (f(this).scrollTop() > 350 ? 'In' : 'Out')](100);
+    });
+});
+
+// open modal
+$(".section-about__btn--show-modal").click(function() {
+    $('#modal').addClass('modal--show');
+    $('body').addClass('hidden');
+});
+
+
+// close modal
+$('.modal__btn--close, .modal__btn--cancel').click(function() {
+    $('#modal').removeClass('modal--show');
+    $('body').removeClass('hidden');
+});
+
+
+// close modal by clicking outside of modal content
+$(document).click(function(event) {
+    if (!$(event.target).closest(".modal__wrapper, .section-about__btn--show-modal").length) {
+        $("#modal").removeClass("modal--show");
+        $('body').removeClass('hidden');
+    }
+});
